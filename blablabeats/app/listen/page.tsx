a778d61ebec994006f285a737cee85e57205e153
Alec Dewitz <atdewitz@gmail.com>
2023-08-05 15:40:37 -0700

    Update transcription on screen

--- blablabeats/app/listen/page.tsx
+++ blablabeats/app/listen/page.tsx
@@ -14,113 +14,111 @@ let options = {
   mimeType: "audio/webm;codecs=pcm",
 };
 
-const startRecording = async () => {
-  console.log("start recording");
-
-  // Assembly AI audio requirments:
-  // WAV PCM16
-  //   A sample rate that matches the value of the sample_rate query param you supply
-  // Single-channel
-  //   100 to 2000 milliseconds of audio per message
-
-  const token = await fetchAssemblyAIRealtimeToken();
-
-  // Handle the data returned by fetchToken here.
-  console.log("captured token: " + token);
-
-  socket = await new WebSocket(
-    `wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000&token=${token}`
-  );
-  const texts = {};
-
-  socket.onmessage = (message) => {
-    let msg = "";
-    const res = JSON.parse(message.data);
-    console.log("res: " + JSON.stringify(res));
-
-    texts[res.audio_start] = res.text;
-    const keys = Object.keys(texts);
-    keys.sort((a, b) => a - b);
-    for (const key of keys) {
-      if (texts[key]) {
-        if (msg.split(" ").length > 6) {
-          msg = "";
-        }
-        msg += ` ${texts[key]}`;
-      }
-    }
-    // captions.innerText = msg;
-    console.log("recorded message: " + msg);
-  };
-
-  socket.onerror = (event) => {
-    console.error(event);
-    socket.close();
-  };
-
-  socket.onclose = (event) => {
-    console.log(event);
-    // captions.innerText = ''
-    socket = null;
-  };
-
-  socket.onopen = async () => {
-    console.log("onopen");
-    const stream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: false,
-    });
-
-    console.log(stream);
-    const RecordRTC = (await import("recordrtc"))
-      .default as typeof RecordRTCType;
-    recorder = new RecordRTC(stream, {
-      type: "audio",
-      mimeType: "audio/webm;codecs=pcm",
-      recorderType: RecordRTC.StereoAudioRecorder,
-      timeSlice: 250,
-      desiredSampRate: 16000,
-      numberOfAudioChannels: 1,
-      bufferSize: 4096,
-      audioBitsPerSecond: 128000,
-      ondataavailable: function (blob) {
-        const reader = new FileReader();
-        reader.onload = () => {
-          const base64data = reader.result;
-          if (socket) {
-            socket.send(
-              JSON.stringify({ audio_data: base64data.split("base64,")[1] })
-            );
-          }
-        };
-        reader.readAsDataURL(blob);
-      },
-    });
-
-    recorder.startRecording();
-  };
-};
-
-const stopRecording = () => {
-  recorder.stopRecording(stopRecordingCallback);
-  console.log(recordedChunks);
-};
-
-// Stops recording and ends real-time session.
-const stopRecordingCallback = () => {
-  socket.send(JSON.stringify({ terminate_session: true }));
-  socket.close();
-  socket = null;
-
-  recorder.destroy();
-  recorder = null;
-};
-
 const IndexPage = () => {
-  const [selectedPodcaster, setSelectedPodcaster] = useState(null);
+  const [transcript, setTranscript] = useState("");
 
-  const handleChange = (selectedOption) => {
-    setSelectedPodcaster(selectedOption);
+  const startRecording = async () => {
+    console.log("start recording");
+
+    // Assembly AI audio requirments:
+    // WAV PCM16
+    //   A sample rate that matches the value of the sample_rate query param you supply
+    // Single-channel
+    //   100 to 2000 milliseconds of audio per message
+
+    const token = await fetchAssemblyAIRealtimeToken();
+
+    // Handle the data returned by fetchToken here.
+    console.log("captured token: " + token);
+
+    socket = await new WebSocket(
+      `wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000&token=${token}`
+    );
+    const texts = {};
+
+    socket.onmessage = (message) => {
+      let msg = "";
+      const res = JSON.parse(message.data);
+      console.log("res: " + JSON.stringify(res));
+
+      texts[res.audio_start] = res.text;
+      const keys = Object.keys(texts);
+      keys.sort((a: any, b: any) => a - b);
+      for (const key of keys) {
+        if (texts[key]) {
+          if (msg.split(" ").length > 6) {
+            msg = "";
+          }
+          msg += ` ${texts[key]}`;
+        }
+      }
+      // captions.innerText = msg;
+      console.log("recorded message: " + msg);
+
+      setTranscript(msg);
+    };
+
+    socket.onerror = (event) => {
+      console.error(event);
+      socket.close();
+    };
+
+    socket.onclose = (event) => {
+      console.log(event);
+      // captions.innerText = ''
+      socket = null;
+    };
+
+    socket.onopen = async () => {
+      console.log("onopen");
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false,
+      });
+
+      console.log(stream);
+      const RecordRTC = (await import("recordrtc"))
+        .default as typeof RecordRTCType;
+      recorder = new RecordRTC(stream, {
+        type: "audio",
+        mimeType: "audio/webm;codecs=pcm",
+        recorderType: RecordRTC.StereoAudioRecorder,
+        timeSlice: 250,
+        desiredSampRate: 16000,
+        numberOfAudioChannels: 1,
+        bufferSize: 4096,
+        audioBitsPerSecond: 128000,
+        ondataavailable: function (blob) {
+          const reader = new FileReader();
+          reader.onload = () => {
+            const base64data = reader.result;
+            if (socket) {
+              socket.send(
+                JSON.stringify({ audio_data: base64data.split("base64,")[1] })
+              );
+            }
+          };
+          reader.readAsDataURL(blob);
+        },
+      });
+
+      recorder.startRecording();
+    };
+  };
+
+  const stopRecording = () => {
+    recorder.stopRecording(stopRecordingCallback);
+    console.log(recordedChunks);
+  };
+
+  // Stops recording and ends real-time session.
+  const stopRecordingCallback = () => {
+    socket.send(JSON.stringify({ terminate_session: true }));
+    socket.close();
+    socket = null;
+
+    recorder.destroy();
+    recorder = null;
   };
 
   return (
@@ -133,6 +131,7 @@ const IndexPage = () => {
             </div>
           </div>
         </div>
+        <div>{transcript}</div>
         <div className="flex items-center justify-center mt-6">
           <button
             onClick={startRecording}
