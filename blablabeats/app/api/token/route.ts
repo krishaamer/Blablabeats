225a46fc55ea29b60d96676827ed7e4dacc85497
Sasha <sashashengyt@gmail.com>
2023-08-05 15:04:09 -0700

    not work

---
+++ blablabeats/app/api/token/route.ts
@@ -0,0 +1,27 @@
+import ky from "ky";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function getAssemblyAIRealtimeToken(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    res.status(405).json({ error: "method not allowed" });
+    return;
+  }
+
+  try {
+    console.log("token...");
+    const token = await ky
+      .post("https://api.assemblyai.com/v2/realtime/token", {
+        json: { expires_in: 3600 },
+        headers: { authorization: process.env.ASSEMBLY_AI_API_KEY },
+      })
+      .json();
+
+    res.json(token);
+  } catch (err) {
+    console.error("assembly ai token error", err);
+    res.status(500).json(err);
+  }
+}
