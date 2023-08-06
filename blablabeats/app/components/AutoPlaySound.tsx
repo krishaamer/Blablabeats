879c36f1c5fb17f0bc58d9caab73c30ebdab898b
misterA <whoismistera@gmail.com>
2023-08-05 23:32:57 -0700

    fix autoplay sound

--- blablabeats/app/components/AutoPlaySound.tsx
+++ blablabeats/app/components/AutoPlaySound.tsx
@@ -2,12 +2,14 @@ import React, { useEffect } from 'react';
 
 function AutoPlaySound({ soundUrl }) {
   useEffect(() => {
+    if(!soundUrl) return;
     const audio = new Audio(soundUrl);
     audio.play();
   }, [soundUrl]);  // Adding soundUrl to the dependency array
 
   return (
     <div>
+      Sound should play on mount!
     </div>
   );
 }
