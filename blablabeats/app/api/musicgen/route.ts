92c5384d12c40224110df8c7abcd58b0b0218ba3
Sasha <sashashengyt@gmail.com>
2023-08-05 18:15:47 -0700

    build success

--- blablabeats/app/api/musicgen/route.ts
+++ blablabeats/app/api/musicgen/route.ts
@@ -1,15 +1,15 @@
 import ky from 'ky'
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextRequest } from 'next/server'
 import Replicate from 'replicate'
 
 import { NextResponse } from 'next/server'
 
-export async function POST(req: NextApiRequest) {
+export async function POST(req: Request | NextRequest) {
   try {
     const replicate = new Replicate({
       auth: process.env.REPLICATE_API_TOKEN ?? '',
     })
-    const { text } = req.body
+    const { text } = await req.json()
 
     const output = await replicate.run(
       'pollinations/music-gen:9b8643c06debace10b9026f94dcb117f61dc1fee66558a09cde4cfbf51bcced6',
